adc1334979330e1c58dfc19277bfe81a5a698925
kaimo313 <kaimo313@foxmail.com>
2025-12-09 16:28:32 +0800

    feat：格式化时间

--- vite-management/src/views/ums/admin.vue
+++ vite-management/src/views/ums/admin.vue
@@ -5,8 +5,16 @@
             <el-table-column prop="username" label="账号"/>
             <el-table-column prop="nickName" label="姓名"/>
             <el-table-column prop="email" label="邮箱"/>
-            <el-table-column prop="createTime" label="添加时间"/>
-            <el-table-column prop="loginTime" label="最后登录"/>
+            <el-table-column label="添加时间">
+                <template v-slot:default="scope">
+                    {{ formateDate(scope.row.createTime) }}
+                </template>
+            </el-table-column>
+            <el-table-column label="最后登录">
+                <template v-slot:default="scope">
+                    {{ formateDate(scope.row.loginTime) }}
+                </template>
+            </el-table-column>
             <el-table-column prop="status" label="是否启用"/>
             <el-table-column label="操作"></el-table-column>
         </el-table>
@@ -37,6 +45,23 @@ getAdminListApi({
         ElMessage.error('获取用户数据列表失败')
     }
 })
+
+const addZero = (num: number) => {
+    return num > 9 ? num : '0' + num
+}
+
+// 格式化时间
+const formateDate = (time: string | undefined) => {
+    if (!time) return '';
+    const date = new Date(time);
+    const year = date.getFullYear();
+    let month = addZero(date.getMonth() + 1);
+    let day = addZero(date.getDate());
+    let hour = addZero(date.getHours());
+    let min = addZero(date.getMinutes());
+    let sec = addZero(date.getSeconds());
+    return  `${year}-${month}-${day} ${hour}:${min}:${sec}`
+}
 
 </script>
 
